fe27eed3684b36f877246ebc494a53198eaa8f5f
Peter Hanley <hanley.peter@gmail.com>
2020-10-24 19:37:55 +1100

    Added winner halo effect

--- src/components/Play.js
+++ src/components/Play.js
@@ -9,31 +9,46 @@ function Play(props) {
     const myPick = props.mine;
     const housePick = props.house;
     const [countDown, setCountDown] = useState("3");
-    const [housePickElement, setElement] = useState("");
+    const [playerPickElement, setPlayerElement] = useState("");
+    const [housePickElement, setHouseElement] = useState("");
     const [resultMessage, setResultMessage] = useState("");
 
-    
+    const [playerWins, setPlayerWins] = useState(false);
+    const [houseWins, setHouseWins] = useState(false);
 
-    function playGame() {
+    function chooseWinner() {
+        if (myPick === "rock" && housePick === "scissors") {
+            setPlayerWins(true);
+            setHouseWins(false);
+        } else if (myPick === "rock" && housePick === "paper") {
+            setHouseWins(true);
+            setPlayerWins(false);
+        } else if (myPick === "scissors" && housePick === "paper") {
+            setPlayerWins(true);
+            setHouseWins(false);
+        } else if (myPick === "scissors" && housePick === "rock") {
+            setHouseWins(true);
+            setPlayerWins(false);
+        } else if (myPick === "paper" && housePick === "rock") {
+            setPlayerWins(true);
+            setHouseWins(false);
+        } else if (myPick === "paper" && housePick === "scissors") {
+            setHouseWins(true);
+            setPlayerWins(false);
+        } else {
+            setHouseWins(false);
+            setPlayerWins(false);
+        }
+    }
+
+    function showWinner() {
 
         const currentScore = props.score;
 
-        if (myPick === "rock" && housePick === "scissors") {
+        if (playerWins) {
             props.setScore(currentScore + 1);
             setResultMessage("You Win");
-        } else if (myPick === "rock" && housePick === "paper") {
-            props.setScore(currentScore - 1);
-            setResultMessage("You Lose");
-        } else if (myPick === "scissors" && housePick === "paper") {
-            props.setScore(currentScore + 1);
-            setResultMessage("You Win");
-        } else if (myPick === "scissors" && housePick === "rock") {
-            props.setScore(currentScore - 1);
-            setResultMessage("You Lose");
-        } else if (myPick === "paper" && housePick === "rock") {
-            props.setScore(currentScore + 1);
-            setResultMessage("You Win");
-        } else if (myPick === "paper" && housePick === "scissors") {
+        } else if (houseWins) {
             props.setScore(currentScore - 1);
             setResultMessage("You Lose");
         } else {
@@ -62,27 +77,39 @@ function Play(props) {
     }
 
     useEffect(() => {
+
+        chooseWinner();
+
         setTimeout(() => (setCountDown("2")), 500);
         setTimeout(() => (setCountDown("1")), 1000);
         setTimeout(() => {
-            setElement(
+            setHouseElement(
                 <div className="pick">
                     <div className="result__desktop pick__title">The House Picked {housePick}</div>
-                    <div className={"pick__item " + setMyPickClass(housePick)} style={{backgroundImage: `url(${setMyPickIcon(housePick)}`}}></div>
+                    <div className={"pick__item " + (houseWins? "winner " : "") + setMyPickClass(housePick)} style={{backgroundImage: `url(${setMyPickIcon(housePick)}`}}></div>
                     <div className="result__mobile pick__title">The House Picked {housePick}</div>
                 </div>
             );
-            playGame();
+            setPlayerElement(
+                <div className="pick">
+                    <div className="result__desktop pick__title">You Picked {myPick}</div> 
+                    <div className={"pick__item " + (playerWins? "winner " : "") + setMyPickClass(myPick)} style={{backgroundImage: `url(${setMyPickIcon(myPick)}`}}></div>   
+                    <div className="result__mobile pick__title">You Picked {myPick}</div> 
+                </div>
+            )
+            showWinner();
         },1500);
-    }, [setElement, setCountDown])
+    }, [setHouseElement, setCountDown, houseWins, playerWins])
 
     return (
         <div className="play-wrapper">
-            <div className="pick">
-                <div className="result__desktop pick__title">You Picked {myPick}</div> 
-                <div className={"pick__item " + setMyPickClass(myPick)} style={{backgroundImage: `url(${setMyPickIcon(myPick)}`}}></div>   
-                <div className="result__mobile pick__title">You Picked {myPick}</div> 
-            </div>
+            {playerPickElement? playerPickElement : 
+                <div className="pick">
+                    <div className="result__desktop pick__title">You Picked {myPick}</div> 
+                    <div className={"pick__item " + setMyPickClass(myPick)} style={{backgroundImage: `url(${setMyPickIcon(myPick)}`}}></div>   
+                    <div className="result__mobile pick__title">You Picked {myPick}</div> 
+                </div>
+            }
             <div className={"result__desktop game__result " + (resultMessage.length? "" : "invisible")}>
                 <h3>{resultMessage}</h3>
                 <Link className="play-again__button " to="/" onClick={()=>(props.setHousePick())}>Play Again</Link>
